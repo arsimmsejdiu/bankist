be0757f176f95f48170abb2641b3752fa2c4023b
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-07-05 14:39:09 +0200

    File structure

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -1,32 +1,13 @@
 import React, { useEffect } from "react";
-import hero from "../img/hero.png";
+import { ScrollTo } from '../components/functionality';
 import Navigation from "../components/navigation";
+
+import hero from "../img/hero.png";
+
 
 function Hero() {
   useEffect(() => {
-    const btnScrollTo = document.querySelector(".btn--scroll-to");
-    const section1 = document.querySelector("#Features");
-
-    btnScrollTo.addEventListener("click", function (e) {
-      const s1coords = section1.getBoundingClientRect();
-      console.log(s1coords);
-
-      console.log(e.target.getBoundingClientRect());
-
-      console.log(
-        "Current scroll (X/Y)",
-        window.pageXOffset,
-        window.pageYOffset
-      );
-
-      console.log(
-        "height/width viewport",
-        document.documentElement.clientHeight,
-        document.documentElement.clientWidth
-      );
-
-      section1.scrollIntoView({ behavior: "smooth" });
-    });
+    ScrollTo();
   });
 
   return (
--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,45 +1,11 @@
 import React, { useEffect } from "react";
+import { StickyNav } from './functionality';
+
 import logo from "../img/logo.png";
 
 function Navigation() {
   useEffect(() => {
-    const nav = document.querySelector(".nav");
-
-    const handleHover = function (e) {
-      if (e.target.classList.contains("nav__link")) {
-        const link = e.target;
-        const siblings = link.closest(".nav").querySelectorAll(".nav__link");
-        const logo = link.closest(".nav").querySelector("img");
-
-        siblings.forEach((el) => {
-          if (el !== link) el.style.opacity = this;
-        });
-        logo.style.opacity = this;
-      }
-    };
-
-    // Passing "argument" into handler
-    nav.addEventListener("mouseover", handleHover.bind(0.5));
-    nav.addEventListener("mouseout", handleHover.bind(1));
-
-    const header = document.querySelector(".header");
-    const navHeight = nav.getBoundingClientRect().height;
-
-    const stickyNav = function (entries) {
-      const [entry] = entries;
-      // console.log(entry);
-
-      if (!entry.isIntersecting) nav.classList.add("sticky");
-      else nav.classList.remove("sticky");
-    };
-
-    const headerObserver = new IntersectionObserver(stickyNav, {
-      root: null,
-      threshold: 0,
-      rootMargin: `-${navHeight}px`,
-    });
-
-    headerObserver.observe(header);
+    StickyNav();
   });
 
   return (
